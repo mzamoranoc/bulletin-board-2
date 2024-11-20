38ec019fa15481c3820a1de1c057ad3a386aba5b
mzamoranoc <mzamoran@chicagobooth.edu>
2024-11-20 21:13:26 +0000

    rails generate devise install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Allow server to be hosted on any URL
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.hosts.clear
   # Allow better_errors to work in online IDE
   config.web_console.allowed_ips = "0.0.0.0/0.0.0.0"
